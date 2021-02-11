06e707ab6fbd4f144cec01ac3a5365c8c6ec6056
Jonatha Lara <jonathalarasilverio@gmail.com>
2021-02-10 21:08:53 -0300

    Starting to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,9 +6,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(3, 5);
-		System.out.println(position);
-
+		Board board = new Board(8, 8);
 	}
 
 }
